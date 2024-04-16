9645ad617a58de06578ee779b3e7992dc6aba73c
TajahH <167078781+TajahH@users.noreply.github.com>
2024-04-16 10:41:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let overtime = hoursOver40 * (hourlyWage*1.5)
 if (hoursWorked < 40){
     console.log(`You earned £ ${hourlyWage*hoursWorked}.`)
 } else if (hoursWorked > 40 && hoursWorked <= 60){
-    console.log(`You earned £ ${overtime} in overtime, and your pay for the first 40 hours is £${(hoursWorked*hourlyWage) - overtime}. Total pay is ${((hoursWorked*hourlyWage) - overtime) + overtime}`)
+    console.log(`You earned £ ${overtime} in overtime, and your pay for the first 40 hours is £${(hoursWorked * hourlyWage) - overtime}. Total pay is ${((hoursWorked*hourlyWage) - overtime) + overtime}`)
 } else { //acounts for cutting off at 60hrs
     console.log ("You can only work a maximum of 60 hours.")
 };
