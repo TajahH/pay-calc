c39abaa72986c0b0fc7d11b7f8666f53eae28a67
TajahH <167078781+TajahH@users.noreply.github.com>
2024-04-16 09:43:47 +0100

    Update index.js
    added extra else if

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const readline = require("readline-sync");
 let hourlyWage = 12
-let overtime = hourlyWage*1.5
 let hoursWorked = parseInt(readline.question(`How many hours did you work this week?: `))
+let hoursOver40 = hoursWorked - 40 
+let overtime = hoursOver40 * (hourlyWage*1.5)
+
 
 if (hoursWorked < 40){
     console.log(`You earned £ ${hourlyWage*hoursWorked}.`)
-} else {
-    console.log(`You earned £ ${hoursWorked*overtime}.`)
-}
+} else if(hoursWorked > 60){
+    console.log ("You can only work a maximum of 60 hours.")
+} else if (hoursWorked > 40 && hoursWorked < 60){
+    console.log(`You earned £ ${overtime} in overtime, and your pay for the first 40 hours is £${hourlyWage * }.`)
+};
+
+//need something else for line 13!!
